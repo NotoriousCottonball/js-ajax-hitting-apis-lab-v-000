c687bdddcea5f281d6baf5670fe4fac67d8e9375
George  Pianka <gpianka1@gmail.com>
2019-04-28 13:21:29 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 const rootURL = 'https://api.github.com';
 
+function getRepositories() {
+    let username = document.getElementById('username').value;
+
+     const req = new XMLHttpRequest();
+    req.addEventListener('load', displayRepositories)
+    req.open('GET', `https://api.github.com/users/${username}/repos`);
+
+     req.send();
+    return false;
+}
 
