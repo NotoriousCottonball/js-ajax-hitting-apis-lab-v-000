f698cad3ef22e054c901497e16223829559be86c
George  Pianka <gpianka1@gmail.com>
2019-04-28 14:07:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,29 +11,39 @@ function getRepositories() {
     req.send();
 }
 
+
 function displayRepositories() {
   const repos = JSON.parse(this.responseText);
   console.log(repos);
   
   const repoList = 
-    '<ul>' 
-    + repos.map( r => {
+    '<ul>' +
+     
+     
+     repos.map( r => {
+        
         const dataUsername = `data-username="${r.owner.login}"`;
         const dataRepository = `data-repository="${r.name}"`;
+        
         return `
             <li>
                 <h2>${r.name}</h2>
                 <a href="${r.html_url}" target="_blank">
                   ${r.html_url}
                 </a><br>
-                <a href="#" ${dataRepository} ${dataUsername} ="getCommits(this)>
+                <a href="#" ${dataRepository} ${dataUsername} onclick="getCommits(this)>
                   Get Commits
                 </a><br>
-                <a href="#" ${dataRepository} ${dataUsername} onclick="getBranches(this)">Get Branches</a>
+                <a href="#" ${dataRepository} ${dataUsername} onclick="getBranches(this)">
+                  Get Branches
+                </a>
             </li>`;
-    })
-    .join('') +
+     }).join('') +
+     
+     
   '</ul>';
+    
+    
     
     document.getElementById('repositories').innerHTML = repoList;
 }
