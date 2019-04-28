5a97984dbf6478c1d1d7102ebc04bc278553e905
George  Pianka <gpianka1@gmail.com>
2019-04-28 14:32:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -94,6 +94,7 @@ function getBranches(el){
   const req = new XMLHttpRequest()
   
   req.addEventListener('load', displayBranches)
+  
   req.open('GET', `https://api.github.com/repos/${username}/${repo}/branches`)
   req.send()
 }
